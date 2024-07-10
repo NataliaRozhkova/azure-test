7f5a569734b14bba55edd64d50c4d6cc42e57554
kobra1789@gmail.com <nat.n.rozhkova@gmail.com>
2024-07-10 11:40:56 +0100

    create webapp for azure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
 import { Component } from '@angular/core';
+import { SecretClient } from '@azure/keyvault-secrets';
+import { InteractiveBrowserCredential } from '@azure/identity';
+import { KeyClient } from '@azure/keyvault-keys';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent   {
   title = 'azur-test';
+
+
+/*
+
+   const credential = new DefaultAzureCredential();
+
+   const vaultName = "<YOUR KEYVAULT NAME>";
+   const url = `https://${vaultName}.vault.azure.net`;
+
+   const client = new KeyClient(url, credential);
+
+   const keyName = "MyKeyName";
+
+   async function main() {
+     const latestKey = await client.getKey(keyName);
+     console.log(`Latest version of the key ${keyName}: `, latestKey);
+     const specificKey = await client.getKey(keyName, { version: latestKey.properties.version! });
+     console.log(`The key ${keyName} at the version ${latestKey.properties.version!}: `, specificKey);
+   }
+
+*/
+
+
+
+    client: KeyClient;
+
+    constructor( ) {
+        const credential = new InteractiveBrowserCredential(
+            {
+                clientId: "65c3394d-d996-47fb-9570-0dde995af2d6",
+                   tenantId: "56cc67ca-d4e8-41ed-a6b2-80437cf6f9dc",
+                  // You may provide a redirectUri based on the redirectUri configured in your Microsoft Entra application:
+                  redirectUri: "http://localhost:4200/"
+            }
+        );
+         const vaultName = "azur-test";
+         const url = 'https://azur-test-keys.managedhsm.azure.net/';
+         this.client = new KeyClient(url, credential);
+        const secretName = "MySecretName";
+
+         console.log( this.client )
+
+
+
+      }
+
+
+
+      async test() {
+
+      const keyName = 'deep-secret';
+
+    const latestKey = await this.client.getKey(keyName);
+      console.log(`Latest version of the key ${keyName}: `, latestKey);
+      const specificKey = await this.client.getKey(keyName, { version: latestKey.properties.version! });
+      console.log(`The key ${keyName} at the version ${latestKey.properties.version!}: `, specificKey);
+
+      }
+
+
+
 }
